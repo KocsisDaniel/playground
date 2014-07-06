d63746b498f3105993b2d23a4a0b3f1ba6bfa3f9
Levente Hudak <levente.hudak@liferay.com>
2014-07-06 16:18:52 +0200

    Source format

--- src/main/java/com/treasurehunter/server/util/LocationUtil.java
+++ src/main/java/com/treasurehunter/server/util/LocationUtil.java
@@ -1,21 +1,9 @@
 package com.treasurehunter.server.util;
 
+/**
+ * @author Levente Hudák
+ */
 public class LocationUtil {
-
-	/**
-	 * Returns distance in miles based on two points given by Latitude and
-	 * Longitude
-	 * @param lat1 Start latitude
-	 * @param long1 Start longitude
-	 * @param lat2 Destination latitude
-	 * @param long2 Destination longitude
-	 * @return Distance in miles as int
-	 */
-	public static int getDistanceInMiles(
-		double lat1, double long1, double lat2, double long2) {
-
-		return (int)(1000D * getDistanceInKilometers(lat1, long1, lat2, long2));
-	}
 
 	/**
 	 * Returns distance in kilometers based on two points given by Latitude and
@@ -40,7 +28,38 @@ public class LocationUtil {
 		return d;
 	}
 
+	public static double getDistanceInKilometersWithAltitude(
+		double lat1, double long1, double alt1, double lat2, double long2,
+		double alt2) {
+
+		double a2 =
+			Math.pow(getDistanceInKilometers(lat1, long1, lat2, long2), 2);
+
+		double b2 = Math.pow((Math.abs(alt2 - alt1) / 1000), 2);
+
+		double c2 = a2 + b2;
+
+		return Math.sqrt(c2);
+	}
+
+	/**
+	 * Returns distance in miles based on two points given by Latitude and
+	 * Longitude
+	 * @param lat1 Start latitude
+	 * @param long1 Start longitude
+	 * @param lat2 Destination latitude
+	 * @param long2 Destination longitude
+	 * @return Distance in miles as double
+	 */
+	public static double getDistanceInMiles(
+		double lat1, double long1, double lat2, double long2) {
+
+		return (
+			_oneMileInKilometers *
+			getDistanceInKilometers(lat1, long1, lat2, long2));
+	}
+
 	private static final double _eQuatorialEarthRadius = 6378.1370D;
 	private static final double _d2r = (Math.PI / 180D);
-
+	private static final double _oneMileInKilometers = 0.621371192;
 }
